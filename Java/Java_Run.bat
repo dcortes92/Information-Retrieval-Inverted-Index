7c6b8fe86dba3ded73e63cf5634b41a97e83f7ab
= <dcortes92@hotmail.com>
2013-06-21 19:04:14 -0600

    Reubicados los arcivos

---
+++ Java/Java_Run.bat
@@ -0,0 +1,1 @@
+java -jar Archivo_Invertido.jar
---
+++ Java/src/archivo_invertido/Archivo_Invertido.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package archivo_invertido;
+
+/**
+ *
+ * @author SirIsaac
+ */
+public class Archivo_Invertido {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Lector l = new Lector();
+        l.leer();
+        
+    }
+}
